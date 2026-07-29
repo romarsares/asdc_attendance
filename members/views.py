0ef409babeb8776bbb3a96a221ef22ffa0134360
romarsares <romarsares@gmail.com>
2026-07-29 23:42:57 +0800

    add members app with member list view and templates

--- members/views.py
+++ members/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from .models import Member
+
+
+@login_required
+def member_list(request):
+    members = Member.objects.select_related('region', 'municipality').all()
+    return render(request, 'members/list.html', {'members': members})
